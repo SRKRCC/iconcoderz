16d762b559f36f5d2bf3498ec14acfa0665a3f35
codevoid048 <williamkeri007@gmail.com>
2026-01-24 21:37:18 +0530

    chunking issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules/react/') || 
-              id.includes('node_modules/react-dom/') || 
-              id.includes('node_modules/react-router-dom/')) {
-            return 'react-vendor';
-          }
-          
           if (id.includes('node_modules/@tanstack/react-query')) {
             return 'react-query';
           }
