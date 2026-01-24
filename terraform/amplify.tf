4d3a6f6d03acd550ab608f9e7b19c635645bb08a
codevoid048 <williamkeri007@gmail.com>
2026-01-25 02:49:33 +0530

    custom url routing removed

--- terraform/amplify.tf
+++ terraform/amplify.tf
@@ -13,11 +13,6 @@ resource "aws_amplify_app" "frontend" {
     VITE_API_BASE_URL = var.api_url
   }
 
-  custom_rule {
-    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|woff2|ttf|map|json|webp)$)([^.]+$)/>"
-    target = "/index.html"
-    status = "200"
-  }
 }
 
 resource "aws_amplify_branch" "main" {
